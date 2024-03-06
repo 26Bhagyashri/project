5c3f6a89f770a09f03ef781f21ef4e5fe8055c26
Bhagyashri Saundarkar <99121521+26Bhagyashri@users.noreply.github.com>
2024-03-06 22:39:55 +0530

    Add files via upload

--- Medilab/Medilab/forms/contact.php
+++ Medilab/Medilab/forms/contact.php
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = 'Online Appointment Form';
+  $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,13 +33,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'Name');
+  $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['date'], 'Appointment Date');
-  $contact->add_message( $_POST['department'], 'Department');
-  $contact->add_message( $_POST['doctor'], 'Doctor');
-  $contact->add_message( $_POST['message'], 'Message');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
